c87802fc09c2c3a8581e09c7c9d2ea32296dc18e
karthikgade7 <31696510+karthikgade7@users.noreply.github.com>
2019-12-06 20:51:25 +0530

    Create CollectionContext.Java

---
+++ src/main/java/com/omnius/app/context/CollectionContext.Java
@@ -0,0 +1,13 @@
+package com.omnius.app.context;
+
+public class CollectionContext {
+    private String collectionName;
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+}
